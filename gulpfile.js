776758af9009cc5f62e1cfeaa46c946404ae0bad
H-1454 <h-1454@H-1454noMacBook-Pro.local>
2017-07-14 09:31:50 +0900

    lint

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+//プラグインの読み込み
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sassLint = require("gulp-sass-lint");
+//タスクの定義
+
+//sassのコンパイル
+gulp.task("sass", function() {
+  // gulp.watch("sass/*.scss", function(){
+    gulp.src("scss/*.scss") //ファイルの参照先を指定
+    .pipe(sass()) //プラグインの実行
+    .pipe(gulp.dest("css")) //処理を行ったファイルの保存先を指定
+  // })
+});
+
+//lintで文法チェック
+gulp.task("lint", function() {
+  gulp.src("scss/*.scss")
+  .pipe(sassLint())
+  .pipe(sassLint.format())
+  .pipe(sassLint.failOnError())
+});
+
+//チェックとコンパイル
+gulp.task("style", function() {
+  gulp.watch("scss/*.scss", function(){
+    gulp.src("scss/*.scss")
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+    .pipe(sass())
+    .pipe(gulp.dest("css"))
+  })
+});
+
+// //自動更新・文法チェック
+// gulp.task('default', () => {
+//     gulp.watch(STYLE_SRC, ['style']);
+// });
+
+
+
+// //lint
+// gulp.task("lint",function(){
+//   return gulp.src(SCSS_SRC)
+//     .pipe(scsslint({"config":".scss-lint.ymi"}));
+// });
